e2bb9e7bdc5b4968470d1896cb35ec265acc50f4
Michael Kroth <mkroth65@gmail.com>
2019-03-28 20:51:00 -0500

    Magic stones starting code

---
+++ magic stones/stones.py
@@ -0,0 +1,16 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+n = int(input())
+print(n, file=sys.stderr)
+for i in input().split():
+    level = int(i)
+    print(level, file=sys.stderr)
+
+# Write an action using print
+# To debug: print("Debug messages...", file=sys.stderr)
+
+print("1")
